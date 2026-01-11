a20faea7b72a058510def01947fbda8b656241b3
Varanslash <odiaenvy@gmail.com>
2026-01-10 22:19:25 -0800

    Kernel 1

---
+++ Kernel.rs
@@ -0,0 +1,22 @@
+use std::io;
+use std::fs;
+use std::io::Result;
+use std::path::Path;
+
+fn main() {
+    let file_path = "VarOS\\VarOSLogger.json";
+    if Path::new(file_path).exists() {
+
+    }
+    else {
+        let contents = "first_boot = True";
+        let init = fs::write(file_path, contents);
+        println!("Initialized {}", file_path);
+        match init {
+            Ok(value) => {}
+            Err(value) => {
+                println!("catastrophic")
+            }
+        }
+    }
+}
