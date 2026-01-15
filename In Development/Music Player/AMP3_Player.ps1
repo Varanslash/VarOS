57a6375f3b83a627681eb038305e0d936c449f90
Varanslash <odiaenvy@gmail.com>
2026-01-14 19:47:24 -0800

    AMP3

---
+++ In Development/Music Player/AMP3_Player.ps1
@@ -0,0 +1,24 @@
+$filename = Read-Host -Prompt "What file would you like to play? (Do not include extension.)"
+
+$fileString = Get-Content -Path "C:\Users\$env:USERNAME\Downloads\VarOS\Userspace\Apps\AMP3_Player\Songs\$filename.amp3" -Raw
+$notes = $fileString -split "\n"
+foreach ($line in $notes) {
+    if ($line.StartsWith("#")) {}
+    elseif ($line.StartsWith("rest")) {
+        $rest = $line -split " "
+        $restval = $rest[1] -as [int]
+        Start-Sleep -Seconds $restval
+    }
+    elseif ($line.StartsWith("print; ")) {
+        $print = $line -split "; "
+        $printstr = $print[1]
+        Write-Host $printstr
+    }
+    elseif ($line -match '^\s*$') {}
+    else {
+        $note = $line -split " "
+        $note1 = $note[0] -as [int]
+        $note2 = $note[1] -as [int]
+        [Console]::Beep($note1, $note2)
+    }
+}
